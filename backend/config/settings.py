b839192846560faa0f00504cbdfc639495503e83
PHI1101 <parkhi1101@naver.com>
2025-11-03 11:08:10 +0900

    "RefineCORSConfiguration"

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -55,7 +55,9 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    'https://ai-agentic-kiosk.vercel.app',
+]
 
 ROOT_URLCONF = 'config.urls'
 
