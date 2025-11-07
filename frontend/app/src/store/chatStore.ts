cd963c1f4ad69d772f0be5d12ed96b00b4ebe7c3
PHI1101 <parkhi1101@naver.com>
2025-11-07 18:04:34 +0900

    "fix:빌드오류해결"

--- frontend/app/src/store/chatStore.ts
+++ frontend/app/src/store/chatStore.ts
@@ -10,10 +10,8 @@ interface ChatState {
   addMessage: (message: Message) => void;
 }
 
-const useChatStore = create<ChatState>((set) => ({
+export const useChatStore = create<ChatState>((set) => ({
   messages: [{ sender: 'bot', text: '안녕하세요! 음성으로 주문을 시작해보세요.' }],
   addMessage: (message) =>
     set((state) => ({ messages: [...state.messages, message] })),
 }));
-
-export default useChatStore;
