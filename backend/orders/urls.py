0c9bd722ad253d162495ee1e6ca080ae9d158fa1
PHI1101 <parkhi1101@naver.com>
2025-11-07 18:30:45 +0900

    "fix:백엔드urls구문오류수정"

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -3,7 +3,9 @@ from .views import ProcessCommandView, ChatWithAIView
 
 urlpatterns = [
     re_path(r'^process-command/?
+]
 , ProcessCommandView.as_view(), name='process-command'),
     re_path(r'^chat/?
+]
 , ChatWithAIView.as_view(), name='chat-with-ai'),
 ]
